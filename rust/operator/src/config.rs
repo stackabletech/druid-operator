f7821d631ff0d7e8a7ebb38eba06b54c40067c0b
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2021-11-08 14:59:42 +0100

    Update rust/operator/src/config.rs

--- rust/operator/src/config.rs
+++ rust/operator/src/config.rs
@@ -6,7 +6,7 @@ pub fn get_jvm_config(role: &DruidRole) -> String {
     -server
     -Duser.timezone=UTC
     -Dfile.encoding=UTF-8
-    -Djava.io.tmpdir=/stackable/tmp/druid
+    -Djava.io.tmpdir=/tmp
     -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager
     -XX:+UseG1GC
     -XX:+ExitOnOutOfMemoryError
