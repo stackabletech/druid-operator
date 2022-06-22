40379a6e731d41aac9c1de4dbc66fd79e607c9b8
Nikolaus Winter <mail@nikolaus-winter.de>
2022-06-22 14:52:00 +0000

    removed CI files (#254)
    # Description
    
    Removed CI files

--- .ci/integration-tests/aws-eks/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone -b "$GIT_BRANCH" https://github.com/stackabletech/druid-operator.git
-(cd druid-operator/ && ./scripts/run_tests.sh --parallel 1)
-exit_code=$?
-./operator-logs.sh druid > /target/druid-operator.log
-exit $exit_code
