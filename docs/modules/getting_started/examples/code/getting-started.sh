e0fa2492824edbf44127cba1398dce453a49cded
Felix Hennig <mail@felixhennig.com>
2022-08-02 09:55:03 +0200

    Updated

--- docs/modules/getting_started/examples/code/getting-started.sh
+++ docs/modules/getting_started/examples/code/getting-started.sh
@@ -18,45 +18,58 @@ echo "Installing Druid and ZooKeeper Operators with Helm"
 helm install commons-operator stackable/commons-operator
 helm install secret-operator stackable/secret-operator
 helm install zookeeper-operator stackable/zookeeper-operator
+helm install hdfs-operator stackable/hdfs-operator
 helm install druid-operator stackable/druid-operator
 # end::helm-install-operators[]
 ;;
 "stackablectl")
 echo "installing Operators with stackablectl"
 # tag::stackablectl-install-operators[]
-stackablectl operator install commons secret zookeeper druid
+stackablectl operator install commons secret zookeeper hdfs druid
 # end::stackablectl-install-operators[]
 ;;
 esac
-
-echo "Adding Helm bitnami repo"
-# tag::add-helm-bitnami-repo[]
-helm repo add bitnami https://charts.bitnami.com/bitnami
-# end::add-helm-bitnami-repo[]
 
 echo "Installing ZooKeeper from zookeeper.yaml"
 # tag::install-zookeeper[]
 kubectl apply -f zookeeper.yaml
 # end::install-zookeeper[]
 
+echo "Awaiting ZooKeeper rollout finish"
+# tag::watch-zookeeper-rollout[]
+kubectl rollout status --watch statefulset/simple-zk-server-default
+# end::watch-zookeeper-rollout[]
+
 echo "Installing HDFS from hdfs.yaml"
 # tag::install-hdfs[]
 kubectl apply -f hdfs.yaml
 # end::install-hdfs[]
 
-echo "Installing the Postgres database"
-# tag::install-bitnami-postgres[]
-helm install druid bitnami/postgresql \
---version=11 \
---set auth.username=druid \
---set auth.password=druid \
---set auth.database=druid
-# end::install-bitnami-postgres[]
+echo "Awaiting HDFS rollout finish"
+# tag::watch-hdfs-rollout[]
+kubectl rollout status --watch statefulset/simple-hdfs-datanode-default
+kubectl rollout status --watch statefulset/simple-hdfs-journalnode-default
+kubectl rollout status --watch statefulset/simple-hdfs-namenode-default
+# end::watch-hdfs-rollout[]
 
 echo "Install DruidCluster from druid.yaml"
 # tag::install-druid[]
 kubectl apply -f druid.yaml
 # end::install-druid[]
+
+echo "Awaiting Druid rollout finish"
+# tag::watch-druid-rollout[]
+kubectl rollout status --watch statefulset/simple-druid-broker-default
+kubectl rollout status --watch statefulset/simple-druid-coordinator-default
+kubectl rollout status --watch statefulset/simple-druid-historical-default
+kubectl rollout status --watch statefulset/simple-druid-middlemanager-default
+kubectl rollout status --watch statefulset/simple-druid-router-default
+# end::watch-druid-rollout[]
+
+
+# next, make sure everything is up and running, then open the port-forwarding
+
+# kubectl port-forward svc/simple-druid-router 8888
 
 
 # TODO I think HDFS is missing. And Postgres might not be necessary, it looks like derby works
