a0144210f7e488a366df56d08bd33d0144a64ab6
Felix Hennig <mail@felixhennig.com>
2022-08-04 09:18:42 +0200

    Updated script

--- scripts/docs_templating.sh
+++ scripts/docs_templating.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Reads a file with variables to insert into templates, and templates all .*.j2 files
+# in the 'docs' directory.
+#
 # dependencies
 # pip install jinja2-cli
 
-templating_vars_file=templating_vars.yaml
+docs_dir=../docs
+templating_vars_file=$docs_dir/templating_vars.yaml
 
 # Check if files need templating
-if [[ $(find | grep .j2\$ | wc -l) -eq "0" ]];
+if [[ $(find "$docs_dir" | grep .j2\$ | wc -l) -eq "0" ]];
 then
   echo "No files need templating, exiting."
   exit
@@ -26,8 +30,11 @@ then
   echo "$templating_vars_file does not exist, cannot start templating."
 fi
 
-for file in $(find | grep .j2\$)
+for file in $(find "$docs_dir" | grep .j2\$)
 do
   new_file_name=$(echo $file | sed 's/\(.*\).j2/\1/g')  # cut of the '.j2'
+  echo "templating $new_file_name"
   jinja2 $file "$templating_vars_file" -o $new_file_name
 done
+
+echo "done"
