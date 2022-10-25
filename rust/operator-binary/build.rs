e7dcd256751e278c65109860c6cf3b445aae665a
Felix Hennig <felix.hennig@stackable.tech>
2022-10-25 16:44:24 +0200

    Removed deploy/crd and generation

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,10 +1,5 @@
-use stackable_druid_crd::DruidCluster;
-use stackable_operator::crd::CustomResourceExt;
-
 fn main() -> Result<(), stackable_operator::error::Error> {
     built::write_built_file().expect("Failed to acquire build-time information");
-
-    DruidCluster::write_yaml_schema("../../deploy/crd/druidcluster.crd.yaml")?;
 
     Ok(())
 }
