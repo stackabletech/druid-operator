8b83265373d335f797a08f9f529da3d1befcd4a7
Malte Sander <contact@maltesander.com>
2022-11-07 16:52:37 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/templates/kuttl/tls/check-tls.sh
+++ tests/templates/kuttl/tls/check-tls.sh
@@ -11,7 +11,7 @@ then
 
   # should work
   echo "[NO_TLS] Test unsecured access"
-  if curl $HOST &> /dev/null
+  if curl "$HOST" &> /dev/null
   then
     echo "[SUCCESS] Could establish connection to unsecured server!"
   else
@@ -27,7 +27,7 @@ then
 
   # should not work without --insecure
   echo "[TLS_ENCRYPTION] Test TLS without trusted CA and without insecure access"
-  if curl $HOST &> /dev/null
+  if curl "$HOST" &> /dev/null
   then
     echo "[ERROR] Could establish connection to untrusted server. Should not be happening!"
     exit 1
@@ -37,7 +37,7 @@ then
 
   # should work with insecure
   echo "[TLS_ENCRYPTION] Test TLS without trusted CA but with insecure access"
-  if curl --insecure $HOST &> /dev/null
+  if curl --insecure "$HOST" &> /dev/null
   then
     echo "[SUCCESS] Could establish connection to server disregarding certificate!"
   else
@@ -47,7 +47,7 @@ then
 
   # should work without insecure but with certificate
   echo "[TLS_ENCRYPTION] Test TLS with trusted certificate"
-  if curl --cacert /tmp/tls/ca.crt $HOST &> /dev/null
+  if curl --cacert /tmp/tls/ca.crt "$HOST" &> /dev/null
   then
     echo "[SUCCESS] Could establish connection to server with trusted certificate!"
   else
@@ -57,7 +57,7 @@ then
 
   # should not work with wrong certificate
   echo "[TLS_ENCRYPTION] Test TLS with untrusted certificate"
-  if curl --cacert /tmp/tls/untrusted-ca.crt $HOST &> /dev/null
+  if curl --cacert /tmp/tls/untrusted-ca.crt "$HOST" &> /dev/null
   then
     echo "[ERROR] Could establish connection to server with untrusted certificate. Should not be happening!"
     exit 1
@@ -73,7 +73,7 @@ then
 
   # Should fail
   echo "[TLS_AUTH] Test insecure access"
-  if curl --insecure $HOST &> /dev/null
+  if curl --insecure "$HOST" &> /dev/null
   then
     echo "[ERROR] Could establish insecure connection to server! This should not be happening!"
     exit 1
@@ -83,7 +83,7 @@ then
 
   # Should fail
   echo "[TLS_AUTH] Test access providing CA"
-  if curl --cacert  $HOST &> /dev/null
+  if curl --cacert  "$HOST" &> /dev/null
   then
     echo "[ERROR] Could establish insecure connection to server! This should not be happening!"
     exit 1
@@ -93,7 +93,7 @@ then
 
   # Should fail
   echo "[TLS_AUTH] Test access providing wrong ca, cert and key"
-  if curl --cacert /tmp/tls/ca.crt --cert /tmp/tls/tls.crt --key /tmp/tls/tls.key $HOST &> /dev/null
+  if curl --cacert /tmp/tls/ca.crt --cert /tmp/tls/tls.crt --key /tmp/tls/tls.key "$HOST" &> /dev/null
   then
     echo "[ERROR] Could establish authenticated connection to server with wrong credentials! This should not be happening!"
     exit 1
@@ -103,7 +103,7 @@ then
 
   # Should work
   echo "[TLS_AUTH] Test access providing correct ca, cert and key"
-  if curl --cacert /tmp/tls_auth/ca.crt --cert /tmp/tls_auth/tls.crt --key /tmp/tls_auth/tls.key $HOST &> /dev/null
+  if curl --cacert /tmp/tls_auth/ca.crt --cert /tmp/tls_auth/tls.crt --key /tmp/tls_auth/tls.key "$HOST" &> /dev/null
   then
     echo "[SUCCESS] Could establish authenticated connection to server!"
   else
