dd12d84717ed4da68d55fd6d1561421af85bbdae
Malte Sander <contact@maltesander.com>
2023-01-10 12:20:13 +0000

    Removed deploy crd folder (#373)
    # Description
    
    fixes https://github.com/stackabletech/druid-operator/issues/197
    
    
    
    Co-authored-by: Malte Sander <malte.sander.it@gmail.com>

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,11 +1,3 @@
-use stackable_druid_crd::DruidCluster;
-use stackable_operator::crd::CustomResourceExt;
-use stackable_operator::error::OperatorResult;
-
-fn main() -> OperatorResult<()> {
+fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
-
-    DruidCluster::write_yaml_schema("../../deploy/crd/druidcluster.crd.yaml")?;
-
-    Ok(())
 }
