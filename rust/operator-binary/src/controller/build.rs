c50c84e7162a795047d24fc39b6b28c64cf05068
Vladislav Supalov <vladislav.supalov@stackable.de>
2023-02-09 10:55:15 +0100

    test: add replacement poc for resource kuttl test

--- rust/operator-binary/src/controller/build.rs
+++ rust/operator-binary/src/controller/build.rs
@@ -186,12 +186,13 @@ pub async fn create_appliable_cluster_resources(
 mod tests {
     use std::sync::Arc;
 
-    use crate::controller::types::AdditionalData;
+    use crate::controller::types::{AdditionalData, AppliableClusterResource};
 
     use super::create_appliable_cluster_resources;
+    use assert_json_diff::{assert_json_matches_no_panic, CompareMode, Config};
     use stackable_druid_crd::{authentication::ResolvedAuthenticationClasses, DruidCluster};
     use stackable_operator::{
-        commons::product_image_selection::ResolvedProductImage,
+        commons::product_image_selection::ResolvedProductImage, kube::ResourceExt,
         product_config::ProductConfigManager,
     };
 
@@ -226,4 +227,172 @@ mod tests {
 
         assert!(result.is_ok(), "we want an ok, instead we got {:?}", result);
     }
+
+    #[tokio::test]
+    async fn test_replace_kuttl_resources_test() {
+        let cluster_cr =
+            std::fs::File::open("test/replace_kuttl_resources_test/druid_cluster.yaml").unwrap();
+        let deserializer = serde_yaml::Deserializer::from_reader(&cluster_cr);
+        let druid_cluster: DruidCluster =
+            serde_yaml::with::singleton_map_recursive::deserialize(deserializer).unwrap();
+        let product_config_manager =
+            ProductConfigManager::from_yaml_file("test/smoke/properties.yaml").unwrap(); // dummy value
+
+        let list_of_appliable_resources = create_appliable_cluster_resources(
+            Arc::new(druid_cluster),
+            AdditionalData {
+                opa_connstr: None,
+                resolved_authentication_classes: ResolvedAuthenticationClasses::new(vec![]),
+                resolved_product_image: ResolvedProductImage {
+                    product_version: "0.1.0".to_string(),
+                    app_version_label: "".to_string(),
+                    image: "".to_string(),
+                    image_pull_policy: "".to_string(),
+                    pull_secrets: None,
+                },
+                zk_connstr: "".to_string(),
+                s3_conn: None,
+                deep_storage_bucket_name: None,
+            },
+            &product_config_manager,
+        )
+        .await
+        .expect("failed to create cluster resources");
+
+        struct StatefulSetYamlTestCases {
+            stateful_set_name: String,
+            yaml_path: String,
+        }
+
+        let stateful_set_tests: Vec<StatefulSetYamlTestCases> = vec![StatefulSetYamlTestCases {
+            stateful_set_name: "druid-resources-historical-default".to_string(),
+            // NOTE: removed line "creation_timestamp: null"
+            yaml_path: "test/replace_kuttl_resources_test/stateful_set_snippets/druid-resources-historical-default.yaml"
+                .to_string(),
+        }];
+
+        // NOTE: this part tries to replicate the "expected yaml fields show up" part of the kuttl test resources/20-assert.yaml
+        for test_entry in stateful_set_tests {
+            let mut checked = false;
+            for entry in list_of_appliable_resources.iter() {
+                if let AppliableClusterResource::RolegroupStatefulSet(the_set, _) = entry {
+                    if the_set.name_unchecked() == test_entry.stateful_set_name {
+                        let generated_json_string = serde_json::to_string(&the_set).unwrap(); // TODO: convert the_set to a yaml, and to a blank dict
+                        let actual_generated_json: serde_json::Value =
+                            serde_json::from_str::<serde_json::Value>(&generated_json_string)
+                                .unwrap();
+
+                        let yaml_file = std::fs::File::open(&test_entry.yaml_path).unwrap();
+                        let deserializer = serde_yaml::Deserializer::from_reader(&yaml_file);
+                        let expected_json: serde_json::Value =
+                            serde_yaml::with::singleton_map_recursive::deserialize(deserializer)
+                                .unwrap();
+
+                        // https://docs.rs/assert-json-diff/latest/assert_json_diff/#partial-matching
+                        // allows FIRST ENTRY to have more fields than SECOND ENTRY, due to Inclusive flag
+                        // we would use assert_json_include! instead here, but it does not allow for useful error string additions
+                        let compare_result = assert_json_matches_no_panic(
+                            &actual_generated_json,
+                            &expected_json,
+                            Config::new(CompareMode::Inclusive),
+                        );
+                        if let Err(error_string) = compare_result {
+                            panic!(
+                                "{} does not contain content of path {}, diff: \n\n{}\n\n",
+                                test_entry.stateful_set_name, test_entry.yaml_path, error_string
+                            );
+                        }
+
+                        checked = true;
+                        break;
+                    }
+                }
+            }
+
+            if !checked {
+                panic!(
+                    "expected stateful set {} was not found",
+                    test_entry.stateful_set_name
+                );
+            }
+        }
+
+        // NOTE: this part tries to replicate the "expected yaml fields show up" part of the kuttl test resources/30-assert.yaml
+        struct ConfigMapTestCases {
+            config_map_name: String,
+            expected_substrings: Vec<String>,
+        }
+
+        let config_map_tests: Vec<ConfigMapTestCases> = vec![
+            ConfigMapTestCases {
+                config_map_name: "druid-resources-historical-default".to_string(),
+                expected_substrings: vec![
+                    "-Xmx2847m".to_string(),
+                    "druid.processing.numThreads=3".to_string(),
+                    "druid.processing.numMergeBuffers=2".to_string(),
+                    "druid.processing.buffer.sizeBytes=161962Ki".to_string(),
+                ],
+            },
+            ConfigMapTestCases {
+                config_map_name: "druid-resources-broker-default".to_string(),
+                expected_substrings: vec!["-Xmx1348m".to_string()],
+            },
+            ConfigMapTestCases {
+                config_map_name: "druid-resources-coordinator-default".to_string(),
+                expected_substrings: vec!["-Xmx1748m".to_string()],
+            },
+            ConfigMapTestCases {
+                config_map_name: "druid-resources-middlemanager-resources-from-role".to_string(),
+                expected_substrings: vec!["-Xmx724m".to_string()],
+            },
+            ConfigMapTestCases {
+                config_map_name: "druid-resources-middlemanager-resources-from-role-group"
+                    .to_string(),
+                expected_substrings: vec!["-Xmx2772m".to_string()],
+            },
+            ConfigMapTestCases {
+                config_map_name: "druid-resources-router-default".to_string(),
+                expected_substrings: vec!["-Xmx1620m".to_string()],
+            },
+        ];
+
+        fn a_contains_b(_a: &str, _b: &str) -> bool {
+            _a.contains(_b)
+        }
+
+        for test_entry in config_map_tests {
+            let mut checked = false;
+            for entry in list_of_appliable_resources.iter() {
+                if let AppliableClusterResource::RolegroupConfigMap(the_cm, _) = entry {
+                    if the_cm.name_unchecked() == test_entry.config_map_name {
+                        let yaml_from_cm = serde_yaml::to_string(&the_cm).unwrap();
+
+                        for substring in test_entry.expected_substrings {
+                            assert!(
+                                a_contains_b(&yaml_from_cm, &substring),
+                                "yaml of cm {} did not contain substring {}",
+                                test_entry.config_map_name,
+                                substring
+                            );
+                        }
+                        checked = true;
+                        break;
+                    }
+                }
+            }
+
+            if !checked {
+                panic!(
+                    "expected config map {} was not found",
+                    test_entry.config_map_name
+                );
+            }
+        }
+    }
+
+    // Felix test suggestions:
+    //  * check values in config maps https://github.com/stackabletech/druid-operator/blob/main/tests/templates/kuttl/resources/30-assert.yaml
+    //  * checking pod template for content https://github.com/stackabletech/druid-operator/blob/main/tests/templates/kuttl/resources/20-assert.yaml
+    //  * testing vs not testing? decided not to, due to overhead: https://github.com/stackabletech/druid-operator/issues/381
+
 }
