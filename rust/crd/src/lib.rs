8063ad321e142a24c71b4a7f87c6b225f192a390
Benedikt Labrenz <benedikt@labrenz.org>
2024-05-02 13:55:53 +0200

    fix doc comment formatting

--- rust/crd/src/lib.rs
+++ rust/crd/src/lib.rs
@@ -310,9 +310,9 @@ pub struct DruidClusterConfig {
 }
 
 /// TODO: Remove once kube-rs is fixed.
-/// Currently using HashSets and BTreeMaps in the schema will result in an invalid CRD that is rejected by the kube-apiserver with 
+/// Currently using HashSets and BTreeMaps in the schema will result in an invalid CRD that is rejected by the kube-apiserver with
 /// error message `Forbidden: uniqueItems cannot be set to true since the runtime complexity becomes quadratic`.
-/// This issue will be fixed in kube-rs by https://github.com/kube-rs/kube/pull/1484
+/// This issue will be fixed in kube-rs by `<https://github.com/kube-rs/kube/pull/1484>`
 pub fn additional_extensions_schema(gen: &mut schemars::gen::SchemaGenerator) -> Schema {
     let mut schema = HashSet::<String>::json_schema(gen);
 
