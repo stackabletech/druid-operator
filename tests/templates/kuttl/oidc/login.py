1c2242cc3cf22a704452fdcf5cc4f8580e781177
Benedikt Labrenz <benedikt@labrenz.org>
2024-05-06 19:54:39 +0200

    introduce test dimension oidc-use-tls

--- tests/templates/kuttl/oidc/login.py
+++ tests/templates/kuttl/oidc/login.py
@@ -1,7 +1,7 @@
 # $NAMESPACE will be replaced with the namespace of the test case.
 
-import json
 import logging
+import os
 import requests
 import sys
 from bs4 import BeautifulSoup
@@ -15,8 +15,9 @@ session = requests.Session()
 
 # Open Druid web UI which will redirect to OIDC login
 login_page = session.get("https://druid-router-default:9088/unified-console.html", verify=False, headers={'Content-type': 'application/json'})
+keycloak_base_url = os.environ['KEYCLOAK_BASE_URL']
 assert login_page.ok, "Redirection from Druid to Keycloak failed"
-assert login_page.url.startswith("http://keycloak:8080/realms/test/protocol/openid-connect/auth?scope=openid+profile+email&response_type=code&redirect_uri=https%3A%2F%2Fdruid-router-default%3A9088%2Fdruid-ext%2Fdruid-pac4j%2Fcallback&state="), \
+assert login_page.url.startswith(f"{keycloak_base_url}/realms/test/protocol/openid-connect/auth?scope=openid+profile+email&response_type=code&redirect_uri=https%3A%2F%2Fdruid-router-default%3A9088%2Fdruid-ext%2Fdruid-pac4j%2Fcallback&state="), \
     "Redirection to Keycloak expected"
 
 # Login to keycloak with test user
