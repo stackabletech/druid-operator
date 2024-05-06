ef06d6efb17814a09584b1ca12c7077962bc1973
Benedikt Labrenz <benedikt@labrenz.org>
2024-05-06 16:41:12 +0200

    fix test steps

--- tests/templates/kuttl/oidc/login.py
+++ tests/templates/kuttl/oidc/login.py
@@ -16,7 +16,7 @@ session = requests.Session()
 # Open Druid web UI which will redirect to OIDC login
 login_page = session.get("https://druid-router-default:9088/unified-console.html", verify=False, headers={'Content-type': 'application/json'})
 assert login_page.ok, "Redirection from Druid to Keycloak failed"
-assert login_page.url.startswith("http://keycloak:8080/realms/test1/protocol/openid-connect/auth?scope=openid+profile+email&response_type=code&redirect_uri=https%3A%2F%2Fdruid-router-default%3A9088%2Fdruid-ext%2Fdruid-pac4j%2Fcallback&state="), \
+assert login_page.url.startswith("http://keycloak:8080/realms/test/protocol/openid-connect/auth?scope=openid+profile+email&response_type=code&redirect_uri=https%3A%2F%2Fdruid-router-default%3A9088%2Fdruid-ext%2Fdruid-pac4j%2Fcallback&state="), \
     "Redirection to Keycloak expected"
 
 # Login to keycloak with test user
