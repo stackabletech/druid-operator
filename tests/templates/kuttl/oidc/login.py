ce28ace2ad62d21b558c7cc1740c1ad15e27c4bf
Benedikt Labrenz <benedikt@labrenz.org>
2024-05-06 21:16:32 +0200

    add integration test for oidc over tls

--- tests/templates/kuttl/oidc/login.py
+++ tests/templates/kuttl/oidc/login.py
@@ -11,11 +11,14 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s: %(message)s",
     stream=sys.stdout)
 
+namespace = sys.argv[1]
+tls = os.environ['OIDC_USE_TLS']
+
 session = requests.Session()
 
 # Open Druid web UI which will redirect to OIDC login
 login_page = session.get("https://druid-router-default:9088/unified-console.html", verify=False, headers={'Content-type': 'application/json'})
-keycloak_base_url = os.environ['KEYCLOAK_BASE_URL']
+keycloak_base_url = f"https://keycloak.{namespace}.svc.cluster.local:8443" if tls == 'true' else f"http://keycloak.{namespace}.svc.cluster.local:8080"
 assert login_page.ok, "Redirection from Druid to Keycloak failed"
 assert login_page.url.startswith(f"{keycloak_base_url}/realms/test/protocol/openid-connect/auth?scope=openid+profile+email&response_type=code&redirect_uri=https%3A%2F%2Fdruid-router-default%3A9088%2Fdruid-ext%2Fdruid-pac4j%2Fcallback&state="), \
     "Redirection to Keycloak expected"
