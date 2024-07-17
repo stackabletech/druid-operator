ecd03030186499ed153fd900155feec4b572ccb1
Nick Larsen <nick.larsen@stackable.tech>
2024-07-17 12:51:51 +0200

    chore(ruff): formatting

--- tests/templates/kuttl/commons/ingestioncheck-tls.py
+++ tests/templates/kuttl/commons/ingestioncheck-tls.py
@@ -12,11 +12,17 @@ import time
 class DruidClient:
     def __init__(self, cert, verify):
         self.session = requests.Session()
-        self.session.headers.update({'Accept': 'application/json', 'Content-Type': 'application/json'})
-        if os.environ['DRUID_OIDC'] == 'true':
-            if os.environ['DRUID_USER'] == '' or os.environ['DRUID_PASSWORD'] == '':
-                sys.exit("Need to provide basic auth user and password when OIDC is enabled.")
-            self.session.auth = HTTPBasicAuth(os.environ['DRUID_USER'], os.environ['DRUID_PASSWORD'])
+        self.session.headers.update(
+            {"Accept": "application/json", "Content-Type": "application/json"}
+        )
+        if os.environ["DRUID_OIDC"] == "true":
+            if os.environ["DRUID_USER"] == "" or os.environ["DRUID_PASSWORD"] == "":
+                sys.exit(
+                    "Need to provide basic auth user and password when OIDC is enabled."
+                )
+            self.session.auth = HTTPBasicAuth(
+                os.environ["DRUID_USER"], os.environ["DRUID_PASSWORD"]
+            )
         self.session.cert = cert
         self.session.verify = verify
 
@@ -33,7 +39,7 @@ class DruidClient:
         return response.text
 
     def post_task(self, url, input):
-        response = self.session.post(url, data=open(input, 'rb'))
+        response = self.session.post(url, data=open(input, "rb"))
         assert response.status_code == 200
         return response.text
 
@@ -77,44 +83,46 @@ elif security == "internal-and-server-tls-and-tls-client-auth":
     cert = ("/tmp/druid-tls/tls.crt", "/tmp/druid-tls/tls.key")
     verify = "/tmp/druid-tls/ca.crt"
 else:
-    sys.exit("Usage: python ./ingestioncheck.py <namespace> <cluster_name> <no-tls|internal-and-server-tls|internal-and-server-tls-and-tls-client-auth>")
+    sys.exit(
+        "Usage: python ./ingestioncheck.py <namespace> <cluster_name> <no-tls|internal-and-server-tls|internal-and-server-tls-and-tls-client-auth>"
+    )
 
 
 druid = DruidClient(cert, verify)
 
-print('''
+print("""
 Query tasks
-===========''')
+===========""")
 tasks = druid.get_tasks(
     url=f"{protocol}://{druid_cluster_name}-coordinator-default-0.{druid_cluster_name}-coordinator-default.{namespace}.svc.cluster.local:{coordinator_port}/druid/indexer/v1/tasks",
 )
 task_count = len(json.loads(tasks))
-print(f'existing tasks: {task_count}')
+print(f"existing tasks: {task_count}")
 
-print('''
+print("""
 Start ingestion task
-====================''')
+====================""")
 ingestion = druid.post_task(
     url=f"{protocol}://{druid_cluster_name}-coordinator-default-0.{druid_cluster_name}-coordinator-default.{namespace}.svc.cluster.local:{coordinator_port}/druid/indexer/v1/task",
-    input='/tmp/druid-quickstartimport.json'
+    input="/tmp/druid-quickstartimport.json",
 )
 task_id = json.loads(ingestion)["task"]
-url_encoded_taskid = urllib.parse.quote(task_id, safe='')
+url_encoded_taskid = urllib.parse.quote(task_id, safe="")
 print(f"TASKID: [{task_id}]")
-print('''
+print("""
 Re-query tasks
-==============''')
+==============""")
 tasks = druid.get_tasks(
     url=f"{protocol}://{druid_cluster_name}-coordinator-default-0.{druid_cluster_name}-coordinator-default.{namespace}.svc.cluster.local:{coordinator_port}/druid/indexer/v1/tasks",
 )
 new_task_count = len(json.loads(tasks))
-print(f'new tasks: {new_task_count}')
-print(f'assert {new_task_count} == {task_count+1}')
+print(f"new tasks: {new_task_count}")
+print(f"assert {new_task_count} == {task_count+1}")
 assert new_task_count == task_count + 1
 
-print('''
+print("""
 Wait for ingestion task to succeed
-======================================''')
+======================================""")
 job_finished = False
 while not job_finished:
     time.sleep(5)
@@ -123,29 +131,33 @@ while not job_finished:
     )
     task_status = json.loads(task)["status"]["statusCode"]
     print(f"Current task status: [{task_status}]")
-    assert task_status == "RUNNING" or task_status == "SUCCESS", f"Taskstatus not running or succeeeded: {task_status}"
+    assert (
+        task_status == "RUNNING" or task_status == "SUCCESS"
+    ), f"Taskstatus not running or succeeeded: {task_status}"
     job_finished = task_status == "SUCCESS"
 
-print('''
+print("""
 Wait for broker to indicate all segments are fully online
-======================================''')
+======================================""")
 broker_ready = False
 while not broker_ready:
     time.sleep(2)
-    broker_ready_rc = druid.check_rc(f"{protocol}://{druid_cluster_name}-broker-default-0.{druid_cluster_name}-broker-default.{namespace}.svc.cluster.local:{broker_port}/druid/broker/v1/readiness")
+    broker_ready_rc = druid.check_rc(
+        f"{protocol}://{druid_cluster_name}-broker-default-0.{druid_cluster_name}-broker-default.{namespace}.svc.cluster.local:{broker_port}/druid/broker/v1/readiness"
+    )
     broker_ready = broker_ready_rc == 200
     print(f"Broker respondend with [{broker_ready_rc}] to readiness check")
 
-print('''
+print("""
 Datasource SQL
-==============''')
+==============""")
 sample_data_size = 39244
 result = druid.query_datasource(
     url=f"{protocol}://{druid_cluster_name}-broker-default-0.{druid_cluster_name}-broker-default.{namespace}.svc.cluster.local:{broker_port}/druid/v2/sql",
-    sql={"query": "select count(*) as c from \"wikipedia-2015-09-12\""},
+    sql={"query": 'select count(*) as c from "wikipedia-2015-09-12"'},
     expected=sample_data_size,
-    iterations=12
+    iterations=12,
 )
-print(f'results: {result}')
-print(f'assert {sample_data_size} == {result}')
+print(f"results: {result}")
+print(f"assert {sample_data_size} == {result}")
 assert sample_data_size == result
