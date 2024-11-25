bcdfc470b532705c2b7acc46870eb1b266b1a587
Benedikt Labrenz <benedikt@labrenz.org>
2024-11-25 11:01:14 +0100

    add unit test for oidc config

--- rust/operator-binary/src/authentication/oidc.rs
+++ rust/operator-binary/src/authentication/oidc.rs
@@ -156,4 +156,77 @@ pub fn add_volumes_and_mounts(
 }
 
 #[cfg(test)]
-mod test {}
+mod tests {
+    use rstest::rstest;
+    use stackable_operator::commons::tls_verification::{Tls, TlsClientDetails};
+
+    use super::*;
+
+    #[rstest]
+    #[case("/realms/sdp")]
+    #[case("/realms/sdp/")]
+    #[case("/realms/sdp/////")]
+    fn test_add_authenticator_config(#[case] root_path: String) {
+        use stackable_operator::commons::{
+            authentication::oidc,
+            tls_verification::{CaCert, TlsServerVerification, TlsVerification},
+        };
+
+        let mut properties = BTreeMap::new();
+        let provider = oidc::AuthenticationProvider::new(
+            "keycloak.mycorp.org".to_owned().try_into().unwrap(),
+            Some(443),
+            root_path,
+            TlsClientDetails {
+                tls: Some(Tls {
+                    verification: TlsVerification::Server(TlsServerVerification {
+                        ca_cert: CaCert::WebPki {},
+                    }),
+                }),
+            },
+            "preferred_username".to_owned(),
+            vec!["openid".to_owned()],
+            None,
+        );
+        let oidc = ClientAuthenticationOptions {
+            client_credentials_secret_ref: "nifi-keycloak-client".to_owned(),
+            extra_scopes: vec![],
+            product_specific_fields: (),
+        };
+
+        add_authenticator_config(&provider, &oidc, &mut properties)
+            .expect("OIDC config adding failed");
+
+        assert_eq!(
+            properties.get("druid.auth.authenticator.Oidc.type"),
+            Some(&Some("pac4j".to_owned()))
+        );
+        assert_eq!(
+            properties.get("druid.auth.pac4j.oidc.oidcClaim"),
+            Some(&Some("preferred_username".to_owned()))
+        );
+        assert_eq!(
+            properties.get("druid.auth.pac4j.oidc.scope"),
+            Some(&Some("openid".to_owned()))
+        );
+        assert_eq!(
+            properties.get("druid.auth.authenticator.Oidc.authorizerName"),
+            Some(&Some("OidcAuthorizer".to_owned()))
+        );
+        assert_eq!(
+            properties.get("druid.auth.authenticatorChain"),
+            Some(&Some("[\"DruidSystemAuthenticator\", \"Oidc\"]".to_owned()))
+        );
+        assert_eq!(
+            properties.get("druid.auth.pac4j.oidc.discoveryURI"),
+            Some(&Some(
+                "https://keycloak.mycorp.org/realms/sdp/.well-known/openid-configuration"
+                    .to_owned()
+            ))
+        );
+
+        assert!(properties.contains_key("druid.auth.pac4j.oidc.clientID"));
+        assert!(properties.contains_key("druid.auth.pac4j.oidc.clientSecret"));
+        assert!(properties.contains_key("druid.auth.pac4j.cookiePassphrase"));
+    }
+}
