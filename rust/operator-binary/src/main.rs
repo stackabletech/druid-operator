4c3e3df01757a2652929e6fa35e239aad92aee79
Nick Larsen <nick.larsen@stackable.tech>
2025-04-07 09:36:58 +0200

    chore: Make telemetry changes consistent with the other operators

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -40,6 +40,9 @@ mod built_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
 
+// TODO (@NickLarsenNZ): Change the variable to `CONSOLE_LOG`
+pub const ENV_VAR_CONSOLE_LOG: &str = "DRUID_OPERATOR_LOG";
+
 #[derive(Parser)]
 #[clap(about, author)]
 struct Opts {
@@ -63,8 +66,7 @@ async fn main() -> anyhow::Result<()> {
                 // TODO (@Techassi): This should be a constant
                 .service_name("druid-operator")
                 .with_console_output((
-                    // TODO (@Techassi): Change to CONSOLE_LOG, create constant
-                    "DRUID_OPERATOR_LOG",
+                    ENV_VAR_CONSOLE_LOG,
                     LevelFilter::INFO,
                     !telemetry_arguments.no_console_output,
                 ))
@@ -81,7 +83,7 @@ async fn main() -> anyhow::Result<()> {
 
                     Settings::builder()
                         // TODO (@Techassi): Change to CONSOLE_LOG or FILE_LOG, create constant
-                        .with_environment_variable("DRUID_OPERATOR_LOG")
+                        .with_environment_variable(ENV_VAR_CONSOLE_LOG)
                         .with_default_level(LevelFilter::INFO)
                         .file_log_settings_builder(log_directory, "tracing-rs.log")
                         .with_rotation_period(rotation_period)
