a2af885f72bf526303570bdb3e805f1d6cc1aa65
Nick <10092581+NickLarsenNZ@users.noreply.github.com>
2025-04-08 11:06:52 +0200

    feat!: Use stackable-telemetry (#703)
    * chore: Replace print_startup_string with structured event
    
    * feat!: Replace log initialization with stackable-telemetry
    
    BREAKING: The log directory variable for the rolling file appender has changed
    
    * chore: Bump ring to 0.11.14 to fix RUSTSEC-2025-0009
    
    * chore: Apply suggestion
    
    Co-authored-by: Nick <10092581+NickLarsenNZ@users.noreply.github.com>
    
    * chore: Make telemetry changes consistent with the other operators
    
    * chore: Update changelog
    
    * chore: Correct changelog entry
    
    ---------
    
    Co-authored-by: Techassi <git@techassi.dev>

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -1,11 +1,11 @@
-use std::sync::Arc;
+use std::{ops::Deref as _, sync::Arc};
 
-use clap::{Parser, crate_description, crate_version};
+use clap::Parser;
 use druid_controller::{DRUID_CONTROLLER_NAME, FULL_CONTROLLER_NAME};
 use futures::StreamExt;
 use stackable_operator::{
     YamlSchema,
-    cli::{Command, ProductOperatorRun},
+    cli::{Command, ProductOperatorRun, RollingPeriod},
     k8s_openapi::api::{
         apps::v1::StatefulSet,
         core::v1::{ConfigMap, Service},
@@ -21,8 +21,10 @@ use stackable_operator::{
     logging::controller::report_controller_reconciled,
     shared::yaml::SerializeOptions,
 };
+use stackable_telemetry::{Tracing, tracing::settings::Settings};
+use tracing::level_filters::LevelFilter;
 
-use crate::crd::{APP_NAME, DruidCluster, OPERATOR_NAME, v1alpha1};
+use crate::crd::{DruidCluster, OPERATOR_NAME, v1alpha1};
 
 mod authentication;
 mod config;
@@ -37,6 +39,9 @@ mod product_logging;
 mod built_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
+
+// TODO (@NickLarsenNZ): Change the variable to `CONSOLE_LOG`
+pub const ENV_VAR_CONSOLE_LOG: &str = "DRUID_OPERATOR_LOG";
 
 #[derive(Parser)]
 #[clap(about, author)]
@@ -54,22 +59,59 @@ async fn main() -> anyhow::Result<()> {
         Command::Run(ProductOperatorRun {
             product_config,
             watch_namespace,
-            tracing_target,
+            telemetry_arguments,
             cluster_info_opts,
         }) => {
-            stackable_operator::logging::initialize_logging(
-                "DRUID_OPERATOR_LOG",
-                APP_NAME,
-                tracing_target,
+            let _tracing_guard = Tracing::builder()
+                // TODO (@Techassi): This should be a constant
+                .service_name("druid-operator")
+                .with_console_output((
+                    ENV_VAR_CONSOLE_LOG,
+                    LevelFilter::INFO,
+                    !telemetry_arguments.no_console_output,
+                ))
+                // NOTE (@Techassi): Before stackable-telemetry was used, the log directory was
+                // set via an env: `DRUID_OPERATOR_LOG_DIRECTORY`.
+                // See: https://github.com/stackabletech/operator-rs/blob/f035997fca85a54238c8de895389cc50b4d421e2/crates/stackable-operator/src/logging/mod.rs#L40
+                // Now it will be `ROLLING_LOGS` (or via `--rolling-logs <DIRECTORY>`).
+                .with_file_output(telemetry_arguments.rolling_logs.map(|log_directory| {
+                    let rotation_period = telemetry_arguments
+                        .rolling_logs_period
+                        .unwrap_or(RollingPeriod::Hourly)
+                        .deref()
+                        .clone();
+
+                    Settings::builder()
+                        // TODO (@Techassi): Change to CONSOLE_LOG or FILE_LOG, create constant
+                        .with_environment_variable(ENV_VAR_CONSOLE_LOG)
+                        .with_default_level(LevelFilter::INFO)
+                        .file_log_settings_builder(log_directory, "tracing-rs.log")
+                        .with_rotation_period(rotation_period)
+                        .build()
+                }))
+                .with_otlp_log_exporter((
+                    "OTLP_LOG",
+                    LevelFilter::DEBUG,
+                    telemetry_arguments.otlp_logs,
+                ))
+                .with_otlp_trace_exporter((
+                    "OTLP_TRACE",
+                    LevelFilter::DEBUG,
+                    telemetry_arguments.otlp_traces,
+                ))
+                .build()
+                .init()?;
+
+            tracing::info!(
+                built_info.pkg_version = built_info::PKG_VERSION,
+                built_info.git_version = built_info::GIT_VERSION,
+                built_info.target = built_info::TARGET,
+                built_info.built_time_utc = built_info::BUILT_TIME_UTC,
+                built_info.rustc_version = built_info::RUSTC_VERSION,
+                "Starting {description}",
+                description = built_info::PKG_DESCRIPTION
             );
-            stackable_operator::utils::print_startup_string(
-                crate_description!(),
-                crate_version!(),
-                built_info::GIT_VERSION,
-                built_info::TARGET,
-                built_info::BUILT_TIME_UTC,
-                built_info::RUSTC_VERSION,
-            );
+
             let product_config = product_config.load(&[
                 "deploy/config-spec/properties.yaml",
                 "/etc/stackable/druid-operator/config-spec/properties.yaml",
