ccf5e4bfe27482f1b95179dd46a3e6c693a6c1b3
Techassi <git@techassi.dev>
2025-10-27 15:21:02 +0100

    chore: Bump Rust dependencies pre-25.11.0

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -9,7 +9,7 @@ use druid_controller::{DRUID_CONTROLLER_NAME, FULL_CONTROLLER_NAME};
 use futures::StreamExt;
 use stackable_operator::{
     YamlSchema,
-    cli::{Command, ProductOperatorRun},
+    cli::{Command, RunArguments},
     k8s_openapi::api::{
         apps::v1::StatefulSet,
         core::v1::{ConfigMap, Service},
@@ -60,18 +60,19 @@ async fn main() -> anyhow::Result<()> {
     match opts.cmd {
         Command::Crd => DruidCluster::merged_crd(DruidClusterVersion::V1Alpha1)?
             .print_yaml_schema(built_info::PKG_VERSION, SerializeOptions::default())?,
-        Command::Run(ProductOperatorRun {
-            product_config,
-            watch_namespace,
+        Command::Run(RunArguments {
             operator_environment: _,
-            telemetry,
-            cluster_info,
+            watch_namespace,
+            product_config,
+            maintenance: _,
+            common,
         }) => {
             // NOTE (@NickLarsenNZ): Before stackable-telemetry was used:
             // - The console log level was set by `DRUID_OPERATOR_LOG`, and is now `CONSOLE_LOG` (when using Tracing::pre_configured).
             // - The file log level was set by `DRUID_OPERATOR_LOG`, and is now set via `FILE_LOG` (when using Tracing::pre_configured).
             // - The file log directory was set by `DRUID_OPERATOR_LOG_DIRECTORY`, and is now set by `ROLLING_LOGS_DIR` (or via `--rolling-logs <DIRECTORY>`).
-            let _tracing_guard = Tracing::pre_configured(built_info::PKG_NAME, telemetry).init()?;
+            let _tracing_guard =
+                Tracing::pre_configured(built_info::PKG_NAME, common.telemetry).init()?;
 
             tracing::info!(
                 built_info.pkg_version = built_info::PKG_VERSION,
@@ -89,7 +90,7 @@ async fn main() -> anyhow::Result<()> {
             ])?;
             let client = stackable_operator::client::initialize_operator(
                 Some(OPERATOR_NAME.to_string()),
-                &cluster_info,
+                &common.cluster_info,
             )
             .await?;
 
