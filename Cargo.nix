32b5285bff361798d648978c3e637fdd57b1b84b
Nick <10092581+NickLarsenNZ@users.noreply.github.com>
2026-03-16 11:34:52 +0100

    chore: Release 26.3.0-rc1 (#803)
    Signed-off-by: Nick Larsen <nick.larsen@stackable.tech>

--- Cargo.nix
+++ Cargo.nix
@@ -9661,7 +9661,7 @@ rec {
       };
       "stackable-druid-operator" = rec {
         crateName = "stackable-druid-operator";
-        version = "0.0.0-dev";
+        version = "26.3.0-rc1";
         edition = "2021";
         crateBin = [
           {
